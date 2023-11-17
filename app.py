1e29eccbab8c5cc562fb803ceb14ce13b176e84f
Moibe <moi_estrello@hotmail.com>
2023-11-16 18:48:01 -0600

    Fix venv python3

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Use the application default credentials.
-cred = credentials.Certificate('glassboilerplate-firebase-adminsdk-ayxb7-a5afc7301f.json')
+cred = credentials.Certificate('glassboilerplate-firebase-adminsdk-ayxb7-905abea505.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
